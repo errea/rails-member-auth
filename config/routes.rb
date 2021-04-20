43c7ae14883d79d5ff9c618526df256ffd71130a
Mihndim <jelsieprisca2016@gmail.com>
2021-04-20 14:15:16 -0400

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :create_tables
-  devise_for :users
+root to: 'posts#index'
+resources :posts, only: [:new, :create, :index] 
+get 'about', to: 'application#about'
+devise_for :users 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
