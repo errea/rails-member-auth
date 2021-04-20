132c5f69bf7a66ed5498a8a5211023d3f54dd104
errea <erreakezy@gmail.com>
2021-04-20 19:32:42 +0100

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :create_tables
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root to: 'posts#index'
+
+  resources :posts
+  devise_for :users
+  resources :likes, only: [:create, :destroy]
+  get 'about', to: 'application#about'
 end
